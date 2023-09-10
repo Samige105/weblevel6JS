7a48d53035a439fb601c881216d8d5f60e0dd33e
Samige105 <116032875+Samige105@users.noreply.github.com>
2023-09-10 22:26:19 +1200

    Add files via upload
    maybe it'll work now...

--- scripts/slideshows.js
+++ scripts/slideshows.js
@@ -4,7 +4,7 @@ let topMovies = [
     {id: 1, title: "The Godfather ", year: 1972, image_url: "images/MovieImages/movie1.jpg"},
     {id: 2, title: "The Dark Knight", year: 2008, image_url: "images/MovieImages/movie2.jpg"},
     {id: 3, title: "12 Angry Men", year: 1957, image_url: "images/MovieImages/movie3.jpg"},
-    {id: 4, title: " Schindler\'s List", year: 1993, image_url: "images/MovieImages/movie4.jpg"},
+    {id: 4, title: "Schindler\'s List", year: 1993, image_url: "images/MovieImages/movie4.jpg"},
 ];
 //------------------------------------------------------------------------------------------------------
 //SLIDE SHOWS
